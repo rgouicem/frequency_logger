3d2d6bf979bf08e01788e6a2b4b30fb695e43daf
Redha Gouicem <redha.gouicem@gmail.com>
2019-12-12 12:21:51 +0100

    test_loop.sh: pin monitor and change duration

--- test_apps/test_loop.sh
+++ test_apps/test_loop.sh
@@ -2,16 +2,17 @@
 
 [ $EUID -ne 0 ] && { echo "Must be root."; exit 1; }
 
-./log_freq.sh 7 traces 0.001 &
+taskset -c 0 ./log_freq.sh 1 traces 0.001 &
 logger=$!
 
-sleep 0.1
+sleep 0.5
 
-test_apps/loop.sh 7 &
+test_apps/loop.sh 1 &
 looper=$!
 
-sleep 0.3
+sleep 0.5
 kill -9 $looper
 
-sleep 0.3
+sleep 0.5
 kill -9 $logger
+wait
