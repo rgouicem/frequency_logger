1ae3f0724a59a3c1b52c6014e1d2ec689db0a4f2
Redha Gouicem <redha.gouicem@gmail.com>
2020-01-11 14:22:43 +0100

    new test that logs all cpus

---
+++ test_apps/log_all_cpus.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -x
+[ $EUID -ne 0 ] && { echo "Must be root."; exit 1; }
+
+taskset -cp 0 $$ &> /dev/null
+
+pids=""
+dirlist=""
+N=$(nproc --all)
+for ((i=0; i<N; i++)) ; do
+    taskset -c 0 ./log_freq.sh $i traces_cpu$i 0.001 &
+    pids+="$! "
+    dirlist+="traces_cpu$i/events "
+done
+
+sleep 1
+
+taskset -c 1 test_apps/loop.sh 1 &
+looper=$!
+
+sleep 1
+
+kill -9 $looper
+
+sleep 1
+
+kill -USR1 $pids
+wait
