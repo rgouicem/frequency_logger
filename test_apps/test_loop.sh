661c79a525377d481fe91c6baaa5336c58227009
Redha Gouicem <redha.gouicem@gmail.com>
2020-04-10 13:23:54 +0200

    test_loop.sh: allow configurable busy loop duration

--- test_apps/test_loop.sh
+++ test_apps/test_loop.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-# Return a nanosecond clock compatible with CLOCK_MONOTONIC_RAW from clock_gettime()
-function now() {
-    sed -nE '/now/s/^now at ([0-9]+).*/\1/p' /proc/timer_list
-}
-
 [ $EUID -ne 0 ] && { echo "Must be root."; exit 1; }
 
+# Default duration of 200 ms
+: ${DURATION:=200000000}
+
+# Pin this script (and its children) to cpu0
 taskset -cp 0 $$ &> /dev/null
 
+# Launch logger on cpu0, monitoring cpu1
 taskset -c 0 ./log_freq.sh 1 traces 0.001 &
 logger=$!
 
 sleep 0.2
 
-taskset -c 1 test_apps/loop > traces/events
+# Launch a busy loop for DURATION ns
+taskset -c 1 test_apps/loop $DURATION > traces/events
 
+# Kill logger 200 ms after busy loop ended
 sleep 0.2
 kill -USR1 $logger
 wait
